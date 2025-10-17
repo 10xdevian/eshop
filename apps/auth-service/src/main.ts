68e0ac42e210be5f53ff4acc3218564bb1b01cf7
nitish <mandalnitish570@gmail.com>
2025-10-17 14:17:12 +0530

    Chnages on setup

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API i am auth service'});
+    res.send({ 'message': 'Hello API i am auth services'});
 });
 
 const server = app.listen(port , ()=> {
